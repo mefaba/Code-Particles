bd7cfe5ab0f6b64625e1d03500ed4a8b607dd5bd
Fatih <31761983+mefaba@users.noreply.github.com>
2020-01-28 12:45:14 +0300

    Create beautifulsoup.py

---
+++ beautifulsoup.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+
+
+
+
+
+def tableDataText(table):       
+    rows = []
+    trs = table.find_all('tr')
+    trs = trs[:11]
+    headerow = [td.get_text(strip=True) for td in trs[0].find_all('th')] # header row
+    if headerow: # if there is a header row include first
+        rows.append(headerow)
+        trs = trs[1:]
+    for tr in trs: # for every table row
+        rows.append([td.get_text(strip=True) for td in tr.find_all('td')]) # data row
+    return rows
