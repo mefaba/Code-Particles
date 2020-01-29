2986c1216d009f764407bd9a172dd1e39d532f01
Fatih <31761983+mefaba@users.noreply.github.com>
2020-01-29 19:34:06 +0300

    Update beautifulsoup.py

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -16,3 +16,28 @@ def tableDataText(table):
     for tr in trs: # for every table row
         rows.append([td.get_text(strip=True) for td in tr.find_all('td')]) # data row
     return rows
+
+#creating empty dataframe with columns
+list_df = pd.DataFrame(columns=('Store Name', 'Address','Info1'))
+def beautifulscrape(html,list_df): 
+    soup = BeautifulSoup(html)   
+    store_name = soup.
+    store_adress = soup.
+    info1= soup.     
+    #This is HOW TO APPEND LİST as ROW into Pandas Dataframe
+    list_df.loc[len(list_df)] = [store_name, store_adress, info1]
+    return list_df
+
+#Selenium
+for city in LISTNAME:  #change according to your listname
+    browser.get("WEBSİTE LINK") 
+    time.sleep(3) #wait for website to open
+    #Get element to you will enter City and click
+    searcher =browser.find_element_by_xpath("XPATH").click()   
+    searcher.send_keys(city)
+    #how to press enter ====> searcher.send_keys(Keys.ENTER) #enter
+    xpath = "XPATH".format(city)  #bonus
+    element= browser.find_element_by_xpath(xpath).click()  
+    storelist_html = browser.find_element_by_xpath("XPATH").get_attribute('outerHTML')#getouterhtml of element that involves storeinformation.
+    beautifulscrape(storelist_html,list_df)     ####.....SCRAPE-BeautifulSoup4.....#####
+    print("LETS GOOO")
