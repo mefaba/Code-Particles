9807563940268caa838627e464eb50bc4912738d
Fatih <31761983+mefaba@users.noreply.github.com>
2020-07-23 21:53:28 +0300

    Create Modern_OOP.js

---
+++ Javascript/Modern_OOP.js
@@ -0,0 +1,113 @@
+//OOP-0
+// factory function make/create
+function createElf(name, weapon) {
+  //we can also have closures here to hide properties from being changed.
+  return {
+    name: name,
+    weapon: weapon,
+    atack() {
+      return 'atack with ' + weapon
+    }
+  }
+}
+const sam = createElf('Sam', 'bow');
+const peter = createElf('Peter', 'bow');
+
+sam.atack()
+
+//OOP-1
+const elfFunctions = {
+  attack: function() {
+    return 'atack with ' + this.weapon
+  }
+}
+function createElf(name, weapon) {
+  //Object.create creates __proto__ link
+  newElf = Object.create(elfFunctions)
+  newElf.name = name;
+  newElf.weapon = weapon
+  return newElf
+}
+
+
+const sam = createElf('Sam', 'bow');
+const peter = createElf('Peter', 'bow');
+sam.attack()
+
+//OOP-2
+
+//Constructor Functions
+function Elf(name, weapon) {
+  this.name = name;
+  this.weapon = weapon;
+}
+
+Elf.prototype.attack = function() { 
+  return 'atack with ' + this.weapon
+}
+const sam = new Elf('Sam', 'bow');
+const peter = new Elf('Peter', 'bow');
+sam.attack()
+
+OOP-3
+class Elf {
+  constructor(name, weapon) {
+    this.name = name;
+    this.weapon = weapon;
+  }
+  attack() {
+    return 'atack with ' + this.weapon
+  }
+}
+
+const fiona = new Elf('Fiona', 'ninja stars');
+console.log(fiona instanceof Elf) // 
+const ben = new Elf('Ben', 'bow');
+fiona.attack()
+
+
+//Binding Options
+//binding with "new"
+function Person(name, age) {
+  this.name = name;
+  this.age =age;
+  console.log(this);
+}
+
+const person1 = new Person('Xavier', 55)
+
+//implicit binding
+const person = {
+  name: 'Karen',
+  age: 40,
+  hi() {
+    console.log('hi' + this.name)
+  }
+}
+
+person.hi()
+
+//explicit binding
+const person3 = {
+  name: 'Karen',
+  age: 40,
+  hi: function() {
+    console.log('hi' + this.setTimeout)
+  }.bind(window)
+}
+
+person3.hi()
+
+// binding in arrow functions
+const person4 = {
+  name: 'Karen',
+  age: 40,
+  hi: function() {
+    var inner = () => {
+      console.log('hi ' + this.name)
+    }
+    return inner()
+  }
+}
+
+person4.hi()
