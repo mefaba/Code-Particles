a4887af099e2e3448b8a55904a78cccba13454c1
Fatih <31761983+mefaba@users.noreply.github.com>
2020-07-23 08:47:27 +0300

    Create Closures.js

---
+++ javascript/Closures.js
@@ -0,0 +1,131 @@
+//Higher Order Functions
+const giveAccessTo = (name) =>
+  'Access Granted to ' + name;
+
+function authenticate(person) {
+  let array = [];
+  // you can add checks here for person.level
+  for (let i = 0; i < 50000; i++) {
+    array.push(i)
+  }
+  return giveAccessTo(person.name)
+}
+
+function letPerson(person, fn) { // ++ We now tell the function what data to use when we call it not when we define it + tell it what to do.
+  if (person.level === 'admin') {
+    return fn(person)
+  } else if (person.level === 'user') {
+    return fn(person)
+  }
+}
+
+function sing(person) {
+  return 'la la la my name is ' + person.name
+}
+
+letPerson({level: 'user', name: 'Tim'}, sing)
+
+// Make a generic multiplyBy HOF that can make other functions like: multiplyByTwo, multiplyByTen and so on...
+const multiplyBy = (num1) => {
+  return function (num2) {
+    return num1 * num2;
+  }
+}
+
+const multiplyByTwo = multiplyBy(2);
+multiplyByTwo(4)
+
+
+//Closures
+function a() {
+  let grandpa = 'grandpa'
+  return function b() {
+    let father = 'father'
+    return function c() {
+      let son = 'son'
+      return `${grandpa} > ${father} > ${son}`
+    }
+  }
+}
+
+a()
+
+//closures and higher order function
+function boo(string) {
+  return function(name) {
+    return function(name2) {
+      console.log(`hi ${name2}`)
+    }
+  }
+}
+
+const boo2 = (string) => (name) => (name2) => console.log(`hi ${name2}`)
+
+boo('hi')('john')('tanya');
+
+// AHH! HOW DOES IT REMEMBER THIS 5 years from now?
+booString = boo2('sing');
+booStringName = booString('John');
+booStringNameName2 = booStringName('tanya')
+
+
+//Closures Continues
+function heavyDuty(item) {
+  const bigArray = new Array(7000).fill('😄')
+  console.log('created!');
+  return bigArray[item]
+}
+
+heavyDuty(699)
+heavyDuty(699)
+heavyDuty(699)
+const getHeavyDuty = heavyDuty2();
+getHeavyDuty(699)
+getHeavyDuty(699)
+getHeavyDuty(699)
+
+// but i dont want to pollute the global namespace..
+function heavyDuty2() {
+  const bigArray = new Array(7000).fill('😄')
+  console.log('created Again!')
+  return function(item) {
+    return bigArray[item]
+  }
+}
+
+/Encapsulation
+// Exercise:
+const makeNuclearButton = () => {
+  let timeWithoutDestruction = 0;
+  const passTime = () => timeWithoutDestruction++;
+  const totalPeaceTime = () => timeWithoutDestruction;
+  const launch = () => {
+    timeWithoutDestruction = -1;
+    return '💥';
+  }
+
+  setInterval(passTime, 1000);
+  return {totalPeaceTime}
+}
+
+const ww3 = makeNuclearButton();
+ww3.totalPeaceTime()
+
+
+//Exercise
+const array = [1,2,3,4];
+for(let i=0; i < array.length; i++) {
+   setTimeout(function(){
+     console.log('I am at index ' + array[i])
+   }, 3000)
+}
+
+
+const array = [1,2,3,4];
+for(var i=0; i < array.length; i++) {
+  (function(closureI) {
+    setTimeout(function(){
+      console.log('I am at index ' + array[closureI])
+    }, 3000)
+  })(i)
+}
